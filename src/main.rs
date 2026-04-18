1fbc3732acf33a3cd2448fff7e25c2efdd96b896
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-04-18 12:13:22 +0100

    refactor(main): replace filesystem traversal with du command for size output

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use std::fs;
-use std::path::Path;
 use std::process::Command;
 
-fn tree() -> Result<(), Box<dyn std::error::Error>> {
-	for dir in fs::read_dir(Path::new("/"))? {
-		let path = dir?.path();
-		match fs::metadata(&path) {
-			Ok(meta) => println!("{}: {:?}", path.display(), meta),
-			Err(_) => println!("{}: Err", path.display())
-		}
-	}
-	Ok(())
-}
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	tree()
+fn main() {
+	let output = Command::new("du")
+		.args(["-sh", "/Users/lenix/Downloads/sysenix"])
+		.output()
+		.unwrap();
+
+	println!("{}", String::from_utf8_lossy(&output.stdout));
 }
