d2df00ff631a7fbe1d85c038534e0ad2889d1809
Lenix <149199196+LenixDev@users.noreply.github.com>
2026-05-11 14:23:14 +0100

    refactor(main): remove unused du command and related imports

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
-use std::process::Command;
-
-
 fn main() {
-	let output = Command::new("du")
-		.args(["-sh", "/Users/lenix/Downloads/sysenix"])
-		.output()
-		.unwrap();
-
-	println!("{}", String::from_utf8_lossy(&output.stdout));
+	
 }
